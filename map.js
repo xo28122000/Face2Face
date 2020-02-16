277e5c4519cb6ccb602e748feb7d375a09ce4d83
xo28122000 <jainam.2000@yahoo.com>
2020-02-16 10:04:59 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,22 +1,22 @@
-import makeMapInteractive from './interactivity.js';
-import addListeners from './listeners.js';
+import makeMapInteractive from "./interactivity.js";
+import addListeners from "./listeners.js";
 import {
   createNewUser,
   pushNewLocation,
-  stopSearch,
-} from './search-functions.js';
-import { MVRS_API_KEY } from './env.js';
+  stopSearch
+} from "./search-functions.js";
+import { MVRS_API_KEY } from "./env.js";
 import {
   handleStartSearch,
-  handleStopSearch,
-} from './search-button-handlers.js';
+  handleStopSearch
+} from "./search-button-handlers.js";
 
 let long;
 let lat;
 let currentlySearching = false;
 
 var platform = new H.service.Platform({
-  apikey: MVRS_API_KEY,
+  apikey: MVRS_API_KEY
 });
 var geocoder = platform.getGeocodingService();
 
@@ -29,18 +29,18 @@ initmap();
 
 function initmap() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
       map = new H.Map(
-        document.getElementById('mapContainer'),
+        document.getElementById("mapContainer"),
         maptypes.vector.normal.map,
         {
           zoom: 18,
           center: {
             lng: long,
-            lat: lat,
-          },
+            lat: lat
+          }
         }
       );
       addListeners(map);
@@ -52,20 +52,21 @@ function initmap() {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
+      map.removeObjects(map.getObjects());
       const marker = new H.map.Marker({
         lng: long,
-        lat: lat,
+        lat: lat
       });
       map.addObject(marker);
 
       pushNewLocation(long, lat);
     });
   } else {
-    console.error('Geolocation is not supported by this browser!');
+    console.error("Geolocation is not supported by this browser!");
   }
 }
 
-document.getElementById('startSearchButton').addEventListener('click', () => {
+document.getElementById("startSearchButton").addEventListener("click", () => {
   if (currentlySearching) {
     handleStopSearch();
   } else {
