d38a1c30a737127d5273bbed56359112470d37df
mats-codes <mats@balticcode.com>
2020-02-15 23:59:04 -0800

    Change

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-export const MVRS_API_KEY = 'wLdvVBCzJvN79alg3s7yoxBdZg4sodnsfY-HgliGIxo';
+export const MVRS_API_KEY = '';
 // export const BASE_URL = 'http://10.3.17.13:5001';
 export const BASE_URL = 'http://localhost:5001';
